892e125c03201f0a8396a9b50d004205d0b6990b
Niklas Linderoos <n.linderoos@gmail.com>
2025-01-03 11:18:17 +0200

    Start dialog system

---
+++ scripts/dialog_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+@export var dialogue_root : Node
+
+@onready var dialogue_box = dialogue_root.get
+
+
+func _ready() -> void:
+    pass
+
+func start_dialogue (name) -> void:
+    pass
