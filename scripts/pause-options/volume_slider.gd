d1400dce39e91127a9f9b557317e886d2125a1cb
Niklas Linderoos <n.linderoos@gmail.com>
2025-01-06 20:27:06 +0200

    Removed test sound effect

--- scripts/pause-options/volume_slider.gd
+++ scripts/pause-options/volume_slider.gd
@@ -5,10 +5,10 @@ var bus_name: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	value = SoundManager.get_volume(bus_name)
 	value_changed.connect(_on_value_changed)
 	
-	value = SoundManager.get_volume(bus_name)
-	
+
 func _on_value_changed(value: float) -> void:
 	SoundManager.set_volume(value, bus_name)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
